86998e9b54afa68c5b2fb1a609a624b354a266c2
Luís Felipe Pinheiro <luipe99@gmail.com>
2023-09-08 11:46:46 -0300

    Some corrections

--- src/app/sala-dental/sala-dental.component.ts
+++ src/app/sala-dental/sala-dental.component.ts
@@ -3,16 +3,19 @@ import { Component } from '@angular/core';
 interface CardPositivo {
   text: string;
   upvotes: number;
+  voted: boolean;
 }
 
 interface CardNegativo {
   text: string;
   upvotes: number;
+  voted: boolean;
 }
 
 interface CardMelhoria {
   text: string;
   upvotes: number;
+  voted: boolean;
 }
 
 @Component({
@@ -33,27 +36,32 @@ export class SalaDentalComponent {
     if (this.cardPositivoText.trim() !== '') {
       const newCard: CardPositivo = {
         text: this.cardPositivoText,
-        upvotes: 0
+        upvotes: 0,
+        voted: false,
       };
       this.cardsPositivos.push(newCard);
       this.cardPositivoText = '';
     }
   }
+
   createCardN() {
     if (this.cardNegativoText.trim() !== '') {
       const newCard: CardNegativo = {
         text: this.cardNegativoText,
-        upvotes: 0
+        upvotes: 0,
+        voted: false,
       };
       this.cardsNegativos.push(newCard);
       this.cardNegativoText = '';
     }
   }
+
   createCardM() {
     if (this.cardMelhoriaText.trim() !== '') {
       const newCard: CardMelhoria = {
         text: this.cardMelhoriaText,
-        upvotes: 0
+        upvotes: 0,
+        voted: false
       };
       this.cardsMelhorias.push(newCard);
       this.cardMelhoriaText = '';
@@ -61,44 +69,10 @@ export class SalaDentalComponent {
   }
 
   upvoteP(cardP: CardPositivo,) {
-    cardP.upvotes++;
-    console.log(cardP.upvotes);
+
+    if (!cardP.voted) {
+      cardP.upvotes++;
+      cardP.voted = true;
+    }
   }
-
-  // CardPositivo: SalaDentalComponent[] = [];
-  // CardNegativo: SalaDentalComponent[] = [];
-  // CardMelhoria: SalaDentalComponent[] = [];
-
-  // createCardPositivo() {
-  //   this.CardPositivo.push(new SalaDentalComponent());
-  // }
-
-  // createCardNegativo() {
-  //   this.CardNegativo.push(new SalaDentalComponent());
-  // }
-
-  // createCardMelhoria() {
-  //   this.CardMelhoria.push(new SalaDentalComponent());
-  // }
-
-  // ngOnInit() {
-  //   const upVote = document.getElementById('upvote') as HTMLButtonElement
-  //   const teste = document.getElementById('fazalgo') as HTMLButtonElement
-  //   const Votes = document.getElementById('calcvotos') as HTMLSpanElement
-
-  //   let clicks = 0;
-
-  //   upVote.addEventListener('click', () => {
-  //     clicks++;
-
-  //     if (clicks >= 1) {
-  //       console.log('Item já votado!');
-  //       return;
-  //     }
-
-  //     console.log(clicks);
-  //     upVote.textContent = clicks.toString();
-
-  //   })
-  // }
 }
